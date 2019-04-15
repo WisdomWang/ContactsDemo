d25c63548a254c91d20fc4fa2257b2929e4d5830
WisdomWang <294814110@qq.com>
2019-04-15 16:32:10 +0800

    add searchbar

--- CAContacts/Controller/ViewController.m
+++ CAContacts/Controller/ViewController.m
@@ -14,10 +14,11 @@
 #import "AddContactsViewController.h"
 #import "ShowContactsViewController.h"
 
-@interface ViewController ()<UITableViewDelegate,UITableViewDataSource,NSFetchedResultsControllerDelegate>
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource,NSFetchedResultsControllerDelegate,UISearchControllerDelegate,UISearchResultsUpdating>
 
 @property (nonatomic,strong) UITableView *tableView;
 @property (nonatomic,strong) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic,strong) UISearchController *searchController;
 
 @end
 
@@ -33,52 +34,99 @@
     UIBarButtonItem *right = [[UIBarButtonItem alloc]initWithCustomView:rightButton];
     self.navigationItem.rightBarButtonItem = right;
     
+    [self loadMyFetchResultsController];
+    
     [self createUI];
-}
-
--(NSFetchedResultsController *)fetchedResultsController
-{
-    if (_fetchedResultsController != nil) {
-        return _fetchedResultsController;
-    }
-
-    ///创建请求
-    NSFetchRequest *request = [[NSFetchRequest alloc]initWithEntityName:@"ContactsEntity"];
-    ///设置请求排序器
-   // request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"namepinyin" ascending:YES]];
-    request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"sectionName" ascending:YES],[NSSortDescriptor sortDescriptorWithKey:@"namepinyin" ascending:YES]];
-    _fetchedResultsController = [[NSFetchedResultsController alloc]initWithFetchRequest:request managedObjectContext:[MyCoreDataManager shareInstace].managerContext sectionNameKeyPath:@"sectionName" cacheName:nil];
-
-    _fetchedResultsController.delegate = self;
-
-    ///执行查询
-    [_fetchedResultsController performFetch:nil];
-
-    [self.tableView reloadData];
-
-    return _fetchedResultsController;
+   
 }
 
  - (void)createUI {
-    
+
+     self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+     
+     //添加搜索栏
+     self.searchController.searchBar.frame = CGRectMake(0, 0, self.view.frame.size.width, 44);
+     self.tableView.tableHeaderView = self.searchController.searchBar;
+     
+     //这是搜索的代理
+     self.searchController.delegate = self;
+     self.searchController.searchResultsUpdater = self;
+     
+     //搜索时背景不暗淡
+     self.searchController.dimsBackgroundDuringPresentation = NO;
+   //  self.searchController.hidesNavigationBarDuringPresentation = NO;
+     self.searchController.obscuresBackgroundDuringPresentation = NO;
+     
     _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, xScreenWidth, xScreenHeight) style:UITableViewStylePlain];
     _tableView.delegate = self;
     _tableView.dataSource = self;
     _tableView.tableFooterView = [[UIView alloc]initWithFrame:CGRectZero];
+    _tableView.tableHeaderView = self.searchController.searchBar;;
     [self.view addSubview:_tableView];
+}
+
+
+-(void)loadMyFetchResultsController {
+    
+    ///创建请求
+    NSFetchRequest *request = [[NSFetchRequest alloc]initWithEntityName:@"ContactsEntity"];
+    ///设置请求排序器
+    request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"sectionName" ascending:YES],[NSSortDescriptor sortDescriptorWithKey:@"namepinyin" ascending:YES]];
+    _fetchedResultsController = [[NSFetchedResultsController alloc]initWithFetchRequest:request managedObjectContext:[MyCoreDataManager shareInstace].managerContext sectionNameKeyPath:@"sectionName" cacheName:nil];
+    
+    _fetchedResultsController.delegate = self;
+    
+    ///执行查询
+    [_fetchedResultsController performFetch:nil];
+    
+    [self.tableView reloadData];
+
+}
+
+#pragma mark - UISearchResultsUpdating 协议
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController
+{
+    //获取输入搜索的内容
+    NSString * text = self.searchController.searchBar.text;
+    
+    //创建请求
+    NSFetchRequest * request = [NSFetchRequest fetchRequestWithEntityName:@"ContactsEntity"];
+    
+    //创建排序器
+    NSSortDescriptor * descriptor = [NSSortDescriptor sortDescriptorWithKey:@"namepinyin" ascending:YES];
+    [request setSortDescriptors:@[descriptor]];
+    
+    //创建谓词
+    NSPredicate * predicate = [NSPredicate predicateWithFormat:@"name contains %@",text];
+    [request setPredicate:predicate];
+    
+    //初始化fetchResulteController
+    self.fetchedResultsController = [[NSFetchedResultsController alloc]initWithFetchRequest:request managedObjectContext:[MyCoreDataManager shareInstace].managerContext sectionNameKeyPath:@"sectionName" cacheName:nil];
+    
+    //执行
+    NSError * error;
+    if (![self.fetchedResultsController performFetch:&error])
+    {
+        NSLog(@"%@",[error localizedDescription]);
+    }
+    
+    //刷新当前tableView
+    [self.tableView reloadData];
+}
+
+- (void)didDismissSearchController:(UISearchController *)searchController
+{
+    
+    //重新设置fetchResultsController的属性
+    [self loadMyFetchResultsController];
+    
+    //刷新当前的tableView
+    [self.tableView reloadData];
+    
 }
 
 -(NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
 
-//    NSMutableArray *toBeReturned = [[NSMutableArray alloc]init];
-//
-//    for(char c = 'A'; c <= 'Z'; c++ ) {
-//
-//        [toBeReturned addObject:[NSString stringWithFormat:@"%c",c]];
-//
-//    }
-//    return toBeReturned;
-    
     return self.fetchedResultsController.sectionIndexTitles;
 }
 
@@ -119,6 +167,7 @@
     ContactsEntity *c = [_fetchedResultsController objectAtIndexPath:indexPath];;
     ShowContactsViewController *vc = [[ShowContactsViewController alloc]init];
     vc.c = c;
+    [self.searchController setActive:NO];
     [self.navigationController pushViewController:vc animated:YES];
     
 }
--- CAContacts/Other/TheHeaderFile.h
+++ CAContacts/Other/TheHeaderFile.h
@@ -17,6 +17,11 @@
 #define xNullString(string) ((![string isKindOfClass:[NSString class]]) || [string isEqualToString:@""] || (string == nil) || [string isEqualToString:@""] || [string isKindOfClass:[NSNull class]] || [[string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] length] == 0)
 
 
+#define xStatusBarHeight    ([[UIApplication sharedApplication] statusBarFrame].size.height)
+#define xNavBarHeight       44.0f
+#define xTopHeight          (xStatusBarHeight + xNavBarHeight)
+
+
 // 十六进制颜色
 
 #define COLOR_WITH_HEX(hexValue) [UIColor colorWithRed:((float)((hexValue & 0xFF0000) >> 16)) / 255.0 green:((float)((hexValue & 0xFF00) >> 8)) / 255.0 blue:((float)(hexValue & 0xFF)) / 255.0 alpha:1.0f]
